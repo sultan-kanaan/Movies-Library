b19964859a2b17949ae25a17efc5d10b8d768bff
sultan-kanaan <sultan.kanaan92@gmail.com>
2022-02-20 22:13:28 +0200

    task 11

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Movies-Library
+
+**Author Name**: sultan kanaan
+
+## WRRC
+![WRRC](./movieData/webRRC.png)
+
+
+## Overview
+we are in the  Movies Library project
+
+## Getting Started
+http://localhost:3000
+
+## Project Features
+-Home Page Endpoint
+-Favorite Page Endpoint
+-page not found error
---
+++ server.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const express = require("express");
+const myData  = require("./movieData/Data.json")
+const app = express();
+
+app.get('/', dataHandler);
+app.get('/favorite',favoritePage);
+app.use("*", notFoundHandler);
+
+function show (title,poster_path,overview){
+    this.title       = title;
+    this.poster_path = poster_path;
+    this.overview    = overview;
+   
+};
+
+function dataHandler(req, res){
+    let result = [];
+    show.data.forEach((value) => {
+        let myShow = new show(value.title,value.poster_path, value.overview);
+        result.push(myShow);
+    });
+    return res.status(200).json(result);
+};
+
+function favoritePage(request, response){
+    return response.status(200).send('Welcome to Favorite Page')
+}
+
+function  notFoundHandler(req,res){
+
+    return res.status(404).send("Sorry, something went wrong");
+}
+
+app.listen(3000, () => {
+    console.log("Listen on 3000");
+});
