f401c6f78c8d3016449a2d456189a4b16acc4baa
Sultan Kanaan <98963963+sultan-kanaan@users.noreply.github.com>
2022-03-03 10:22:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -138,7 +138,7 @@ function updateFavmovie(req, res) {
     const id = req.params.id;
     const movie = req.body;
     const sql = `UPDATE postgres SET title=$1, release_date=$2, poster_path=$3, overview=$4, comment=$5 WHERE id=$6  RETURNING *;`
-    const values = [movie.title, movie.release_date, movie.poster_path, movie.overview, movie.comment];
+    const values = [movie.title, movie.release_date, movie.poster_path, movie.overview, movie.comment, id];
     client.query(sql, values).then(data => {
         return res.status(200).json(data.rows);
     }).catch(error => {
